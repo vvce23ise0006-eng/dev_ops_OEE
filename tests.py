e8dc73fa6ea80df0e9e01fb3d1c63f3b8ed918dd
Rohith Aradhya B S <vvce23ise0006@vvce.ac.in>
2026-05-23 11:35:32 +0530

    Add initial tests for home and health endpoints

---
+++ tests.py
@@ -0,0 +1,16 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home(client):
+    res = client.get('/')
+    assert res.status_code == 200
+
+def test_health(client):
+    res = client.get('/health')
+    assert res.status_code == 200
